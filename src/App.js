91f981c3eb3b755466d200777a1302e7070a74f3
Resham011 <reshamgithub@gmail.com>
2024-07-24 18:05:40 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, { useEffect } from "react";
+import {apiUrl, filterData} from "./data"
+import Navbar from "./components/Navbar"
+import Filter  from "./components/Filter"
+import Cards from "./components/Cards"
+import Spinner from "./components/Spinner"
+import { useState } from "react";
+import { toast } from "react-toastify";
+
+const App = () => {
+     const [courses, setCourses] = useState([]);
+     const [loading, setLoading] = useState(true);
+     const [category, setCategory] = useState(filterData[0].title);
+
+     async function fetchData () {
+          setLoading(true);
+
+          try{
+               const response = await fetch(apiUrl);
+               const output = await response.json();
+
+               // save the data into variable
+               setCourses(output.data);
+          }
+          catch(error){
+               toast.error("Something went wrong");            
+               
+          }
+
+          setLoading(false);
+     }
+
+     useEffect( () => {
+          fetchData();
+     }, [] )
+
+     return (
+          <div className="min-h-screen flex flex-col bg-bgDark2">
+
+               <div>
+                    <Navbar></Navbar>
+               </div>
+
+               <div className="bg-bgDark2">
+                    <div>
+                         <Filter 
+                         filterData = {filterData} 
+                         category = {category} 
+                         setCategory = {setCategory} ></Filter>
+                    </div>
+
+                    <div className="w-11/12 max-w-[1200px] mx-auto flex flex-wrap justify-center items-center min-h-[50vh]">
+                         {
+                              // jo categoray hai vhi card show karungi na isisliye yeh category pass kiya 
+                              loading ? (<Spinner/>) : (<Cards courses={courses} category = {category}/>)
+                         }
+                    </div>
+               </div>
+               
+               
+          </div>
+     );
+};
+
+export default App;
---
+++ src/components/Cards.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import Card from "./Card"
+
+const Cards = (props) => {
+     let courses = props.courses;
+     let category = props.category;
+
+     const [lickedCourses, setLikedCourses] = useState([]);     
+
+     function getCourses() {
+          if(category === "All"){
+               let allCourses = [];
+
+               Object.values(courses).forEach( courseCategory => {
+                    courseCategory.forEach( courseData => {
+                         allCourses.push(courseData);
+                    })
+               })
+               return allCourses;
+          }
+          else{
+               return courses[category];
+          }         
+     }
+
+     return (
+          <div className='flex flex-wrap justify-center gap-4 mb-4'>
+               {
+                    getCourses().map( (course) => (
+                              <Card key={course.id} 
+                              course = {course} 
+                              setLikedCourses = {setLikedCourses}
+                              lickedCourses = {lickedCourses}
+                               />
+                         )                         
+                    )
+               }
+          </div>
+     )
+};
+
+export default Cards;
